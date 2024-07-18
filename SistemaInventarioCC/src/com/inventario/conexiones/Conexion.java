7a3af0c61584d2b8ebd0d3211de76d0ed8e07b9c
valvsp <vvsocorro@urbe.edu.ve>
2024-07-18 13:47:54 -0400

    Ordené conexión de lote

--- SistemaInventarioCC/src/com/inventario/conexiones/Conexion.java
+++ SistemaInventarioCC/src/com/inventario/conexiones/Conexion.java
@@ -2,10 +2,7 @@ package com.inventario.conexiones;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
-import com.inventario.tablas.Lote;
 
 public class Conexion {
 
@@ -50,80 +47,6 @@ public class Conexion {
 		
 		return conexion;
 	}
-	
-	public boolean AgregarLote(Lote a) {
-		
-		try {
-			
-			String query = "INSERT INTO `lote`(`ID_Lote`, `Fecha_Ent`, `Fecha_Venc_Lote`, `Precio_Lote`, `Contenido`, `Proveedor_ID`) VALUES (?,?,?,?,?,?)";
-			
-			PreparedStatement st = conectar().prepareStatement(query);
-			
-			st.setInt(1, a.getID_Lote());
-			st.setDate(2, a.getFecha_Ent());
-			st.setDate(3, a.getFecha_Venc_Lote());
-			st.setFloat(4, a.getPrecio_Lote());
-			st.setString(5, a.getContenido());
-			st.setInt(6, a.getProveedor_ID());
-			
-			st.executeUpdate();
-			
-			return true;
-			
-		} catch(SQLException e) {
-			
-			e.printStackTrace();
-			return false;
-		} finally {
-			
-			try {
-				
-				if(conectar()!=null) {
-					conectar().close();
-				}
-				
-			} catch(SQLException e) {
-				
-				e.printStackTrace();
-			}
-			
-		}
-	}
-	
-	public boolean EliminarLote(Lote b) {
-		
-		try {
-			
-			String query = "DELETE FROM `lote` WHERE `ID_Lote`= ?";
-			
-			PreparedStatement st = conectar().prepareStatement(query);
-			
-			st.setInt(1, b.getID_Lote());
-			
-			st.executeUpdate();
-			
-			return true;
-			
-		} catch(SQLException e) {
-			
-			e.printStackTrace();
-			return false;
-			
-		} finally {
-			
-			try {
-				
-				if(conectar()!=null) {
-					conectar().close();
-				}
-				
-			} catch(SQLException e) {
-				
-				e.printStackTrace();
-			}
-			
-		}
-	}
-	
+
 	
 }
