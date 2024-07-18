5d9b37a18e06e230889c1343181074f527e57e90
valvsp <vvsocorro@urbe.edu.ve>
2024-07-18 13:13:04 -0400

    añadí pedidos, proveedor

--- SistemaInventarioCC/src/com/inventario/tablas/Pedido.java
+++ SistemaInventarioCC/src/com/inventario/tablas/Pedido.java
@@ -1,7 +1,53 @@
 package com.inventario.tablas;
 
+import com.inventario.conexiones.Conexion;
+
 public class Pedido {
 
-	//hola
+	String RIF, Nombre, Email, Tlf, Ubicacion;
+	
+	Conexion c = new Conexion();
+
+	public String getRIF() {
+		return RIF;
+	}
+
+	public void setRIF(String rIF) {
+		RIF = rIF;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getTlf() {
+		return Tlf;
+	}
+
+	public void setTlf(String tlf) {
+		Tlf = tlf;
+	}
+
+	public String getUbicacion() {
+		return Ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		Ubicacion = ubicacion;
+	}
+	
+	
 	
 }
